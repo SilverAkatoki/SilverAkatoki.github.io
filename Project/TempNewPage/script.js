b983d7a48f4c17de0f4351192a9650e762850d89
SilverAkatoki <3125295783@qq.com>
2024-06-21 02:02:37 +0800

    按照 TS 标准格式化 TS 文件, 并重新生成了新的 JS 文件

--- Project/TempNewPage/script.js
+++ Project/TempNewPage/script.js
@@ -1,15 +1,15 @@
-var body_container = document.getElementById("body_container");
-var title_container = document.getElementById("title_container");
-var head_container = document.getElementById("head_container");
-var navigation_bar = document.getElementById("navigation_bar");
-var last_update_date_label = document.getElementById("last_update_date");
-var last_update_date_distance_label = document.getElementById("last_update_date_distance");
+var bodyContainer = document.getElementById("body_container");
+var titleContainer = document.getElementById("title_container");
+var headContainer = document.getElementById("head_container");
+var navigationBar = document.getElementById("navigation_bar");
+var lastUpdateDateLabel = document.getElementById("last_update_date");
+var lastUpdateDateDistanceLabel = document.getElementById("last_update_date_distance");
 /**设置 / 修改底栏位置*/
 var fixFooterPosition = function () {
-    body_container.style.minHeight = "".concat(window.innerHeight
-        - title_container.offsetHeight
-        - head_container.offsetHeight
-        - navigation_bar.offsetHeight, "px");
+    bodyContainer.style.minHeight = "".concat(window.innerHeight
+        - titleContainer.offsetHeight
+        - headContainer.offsetHeight
+        - navigationBar.offsetHeight, "px");
 };
 fixFooterPosition();
 window.addEventListener("resize", function () {
@@ -18,16 +18,17 @@ window.addEventListener("resize", function () {
 });
 /* ---------------------------------- 日期处理 ---------------------------------- */
 /**当前时间*/
-var cur_date = new Date();
+var curDate = new Date();
 /**上一次更新时间*/
-var last_update_date = new Date("2024-06-20");
+var lastUpdateDate = new Date("2024-06-20");
 /**将 `Date` 类型变为 `"YYYY.MM.DD"` 型的字符串 */
-var formatDate = function (date_obj) {
-    return "".concat(date_obj.getFullYear(), ".").concat(date_obj.getMonth() + 1, ".").concat(date_obj.getDate());
+var formatDate = function (dateObj) {
+    return "".concat(dateObj.getFullYear(), ".").concat(dateObj.getMonth() + 1, ".").concat(dateObj.getDate());
 };
+/**计算更新天数间隔 */
 var calcDayDistance = function (cur_date, last_update_date) {
-    var millisecond_distance = Math.abs(cur_date.getTime() - last_update_date.getTime());
-    return Math.ceil(millisecond_distance / (1000 * 3600 * 24)).toString();
+    var millisecondDistance = Math.abs(cur_date.getTime() - last_update_date.getTime());
+    return Math.ceil(millisecondDistance / (1000 * 3600 * 24)).toString();
 };
-last_update_date_label.innerHTML = formatDate(last_update_date);
-last_update_date_distance_label.innerHTML = calcDayDistance(cur_date, last_update_date);
+lastUpdateDateLabel.innerHTML = formatDate(lastUpdateDate);
+lastUpdateDateDistanceLabel.innerHTML = calcDayDistance(curDate, lastUpdateDate);
